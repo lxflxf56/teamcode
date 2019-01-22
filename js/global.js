3658bf63199dff0bbea249b292f904e65ff9b701
lxflxf56 <501313018@qq.com>
2019-01-22 18:32:05 +0800

    v0.0.2 跳转bug修复

--- js/global.js
+++ js/global.js
@@ -1,7 +1,5 @@
 var hostname='http://localhost:8080';
 var Authorization=sessionStorage.Authorization;
-var app = angular.module("myApp",[]);
-app.config(['$locationProvider', function($locationProvider) {
-    $locationProvider.html5Mode(true);
-}]);
+
+
 
